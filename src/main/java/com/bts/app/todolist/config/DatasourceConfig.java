efee1380370608092d89627c9eb470cb24c3d6b3
ida.sufiana <ida.sufiana@consultant.ioh.co.id>
2025-01-23 15:45:19 +0700

    update config

---
+++ src/main/java/com/bts/app/todolist/config/DatasourceConfig.java
@@ -0,0 +1,33 @@
+package com.bts.app.todolist.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatasourceConfig {
+
+    @Autowired
+    Environment env;
+
+    @Bean(name = "app-datasource")
+    public DataSource appDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
+        dataSource.setUrl(env.getProperty("spring.datasource.url"));
+        dataSource.setUsername(env.getProperty("spring.datasource.username"));
+        dataSource.setPassword(env.getProperty("spring.datasource.password"));
+        return dataSource;
+    }
+
+    @Bean(name = "appJdbcTemplate")
+    public JdbcTemplate appJdbcTemplate(@Qualifier("app-datasource") DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}
