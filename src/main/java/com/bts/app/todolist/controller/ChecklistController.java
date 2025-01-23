2bb781ea8cc474ee1c6f3355e159b1a59e6e0a9c
ida.sufiana <ida.sufiana@consultant.ioh.co.id>
2025-01-23 15:52:57 +0700

    add checklist service

--- src/main/java/com/bts/app/todolist/controller/ChecklistController.java
+++ src/main/java/com/bts/app/todolist/controller/ChecklistController.java
@@ -1,11 +1,54 @@
 package com.bts.app.todolist.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.bts.app.todolist.dto.ItemDto;
+import com.bts.app.todolist.exception.BTSException;
+import com.bts.app.todolist.model.CheckList;
+import com.bts.app.todolist.model.Item;
+import com.bts.app.todolist.service.ChecklistService;
+import com.bts.app.todolist.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/v1/Checklist")
-public class ChecklistController {
+public class ChecklistController extends BaseController{
+
+    @Autowired
+    ChecklistService checklistService;
+
+    @PreAuthorize("hasRole('USER','ADMIN')")
+    @GetMapping("/{id}")
+    public Object Get(@PathVariable Long id) throws Throwable {
+        Optional<CheckList> product = checklistService.get(id);
+        if(product.isPresent()){
+            return success(checklistService.get(id).get());
+        }else {
+            throw new BTSException("no data found");
+        }
+
+    }
+
+    @PreAuthorize("hasRole('USER','ADMIN')")
+    @PostMapping("/update/{id}")
+    public Object Update(@PathVariable Long id, @RequestBody ItemDto dto) throws Throwable {
+        return success(checklistService.update(id, dto));
+
+    }
+
+    @PreAuthorize("hasRole('USER','ADMIN')")
+    @PostMapping("/add")
+    public Object add(@RequestBody ItemDto dto) throws Throwable {
+        return success(checklistService.createFromDto(dto));
+    }
+
+    @PreAuthorize("hasRole('USER','ADMIN')")
+    @DeleteMapping("/delete/{id}")
+    public Object add(@PathVariable Long id) throws Throwable {
+        return success(checklistService.delete(id));
+    }
 
 
 }
---
+++ src/main/java/com/bts/app/todolist/service/ChecklistService.java
@@ -0,0 +1,77 @@
+package com.bts.app.todolist.service;
+
+import com.bts.app.todolist.dto.ItemDto;
+import com.bts.app.todolist.exception.BTSException;
+import com.bts.app.todolist.model.CheckList;
+import com.bts.app.todolist.model.Item;
+import com.bts.app.todolist.repository.ChecklistRepository;
+import com.bts.app.todolist.repository.ItemRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.Optional;
+
+@Service
+public class ChecklistService {
+
+    @Autowired
+    ChecklistRepository checklistRepository;
+
+    public CheckList createFromDto(ItemDto dto) throws Throwable {
+        try {
+            CheckList entity = new CheckList();
+            BeanUtils.copyProperties(dto, entity);
+            entity.setCreatedDate(new Date());
+            checklistRepository.save(entity);
+            return entity;
+        } catch (Throwable var5) {
+            Throwable $ex = var5;
+            throw $ex;
+        }
+    }
+
+    public Optional<CheckList> get(Long id) throws Throwable {
+        try {
+            return checklistRepository.findById(id);
+        } catch (Throwable var3) {
+            Throwable $ex = var3;
+            throw $ex;
+        }
+    }
+
+    public CheckList update(Long id, ItemDto dto) throws Throwable {
+        try {
+            CheckList entity = new CheckList();
+            if(get(id).isPresent()){
+                entity = checklistRepository.findById(id).get();
+                BeanUtils.copyProperties(dto, entity);
+                entity.setUpdatedDate(new Date());
+                return checklistRepository.save(entity);
+            }else{
+                throw new BTSException("not found");
+            }
+        } catch (Throwable var5) {
+            Throwable $ex = var5;
+            throw $ex;
+        }
+    }
+
+    public CheckList delete(Long id) throws Throwable {
+        try {
+            CheckList entity = new CheckList();
+            if(get(id).isPresent()) {
+                entity = checklistRepository.findById(id).get();
+                checklistRepository.delete(entity);
+                return entity;
+            }else {
+                throw new BTSException("data not found");
+            }
+
+        } catch (Throwable var3) {
+            Throwable $ex = var3;
+            throw $ex;
+        }
+    }
+}
